26e3b7744d66447ead06616e6107b981ca79d662
tomek <twitkowski98@gmail.com>
2025-09-23 11:10:42 +0200

    k actions generalisation

---
+++ src/rml_generalized.py
@@ -0,0 +1,419 @@
+"""Core model file defining Encoder, Decoder, UtilityFunction, and Agent classes."""
+from contextlib import nullcontext
+import sys, os
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_dir)
+
+from config import Config as cfg
+import random
+import time
+
+import tensorflow as tf
+import numpy as np
+
+if 'PRECISION' not in os.environ:
+    os.environ['PRECISION'] = '32'
+
+if os.environ['PRECISION'] == '64':
+    tf_float = tf.float64
+    tf_int = tf.int64
+elif os.environ['PRECISION'] == '32':
+    tf_float = tf.float32
+    tf_int = tf.int32
+else:
+    raise NotImplementedError(f"Unsupported precision: {os.environ['PRECISION']}")
+
+
+DEBUG: bool = False
+
+seed_value = int(os.environ['train_log_name'].split('_')[-1].split('.')[0])
+print('SEED: ', seed_value)
+time.sleep(0.1)
+os.environ['PYTHONHASHSEED'] = str(seed_value)
+random.seed(seed_value)
+np.random.seed(seed_value)
+tf.random.set_seed(seed_value)
+
+def double_tanh(x, a=5, b=2.5):
+    return (tf.tanh(x*a+b) + tf.tanh(x*a-b))/2.
+
+class SharpeRatio(tf.keras.layers.Layer):
+    """
+    Computes Sharpe Ratio or related measures given the final position and price data.
+    """
+    @staticmethod
+    def immediate_returns(inp):
+        B, A, F = inp[:, :1], inp[:, 1:2], inp[:, -1:]
+        dP_perc = (B[1:] - B[:-1]) / B[:-1]
+        dF = F[1:] - F[:-1]
+        c = tf.reduce_mean((A - B) / B)
+        return dP_perc * F[:-1] - c * tf.abs(dF)
+
+    def __init__(self, lewar: int = cfg.lewar, with_swap: bool = False):
+        super(SharpeRatio, self).__init__()
+        self.lewar = lewar
+        self.with_swap = with_swap
+        self.type = cfg.reward_type
+
+    @tf.function
+    def call(self, inp):
+        if self.type == 'custom':
+            spreads_perc = tf.reduce_mean((inp[:,1] - inp[:,0]) / inp[:,0], 0)
+            trans_cost = tf.math.pow(
+                1 - spreads_perc*self.lewar/2,
+                tf.reduce_sum(tf.abs(inp[1:, -1] - inp[:-1, -1]))
+            )
+            dA = inp[1:,1] - inp[:-1,1]
+            add_ret = tf.reduce_sum(dA*inp[:-1,2]/inp[:-1,1]) * self.lewar
+            general_ret = (1 + add_ret)*trans_cost
+            if self.with_swap:
+                point_perc = 0.00001/1.1
+                minutes_per_day = 60*24
+                long_swap_per_day = 7*point_perc
+                n_pos = tf.reduce_sum(tf.abs(inp[:,2]))*self.lewar
+                swap_factor = ((1-long_swap_per_day)**(1/minutes_per_day))**(n_pos)
+                general_ret = general_ret * swap_factor
+            return general_ret
+        elif self.type == 'cumulative':
+            return tf.reduce_sum(self.immediate_returns(inp), axis=1)
+        elif 'sharpe' in self.type:
+            imm = self.immediate_returns(inp)
+            return tf.reduce_mean(imm)/tf.math.reduce_std(imm)
+        else:
+            raise NotImplementedError()
+
+class Encoder(tf.keras.Model):
+    """
+    M_E: compresses the input into a latent representation.
+    """
+    @staticmethod
+    def min_max(t):
+        ma = tf.math.reduce_max(t, axis=1)
+        mi = tf.math.reduce_min(t, axis=1)
+        mi = tf.transpose(tf.reshape(tf.concat([mi]*t.shape[1], 0), (t.shape[1], -1)))
+        ma = tf.transpose(tf.reshape(tf.concat([ma]*t.shape[1], 0), (t.shape[1], -1)))
+        return t - mi
+
+    @staticmethod
+    def conv(x, filters, length, expand=False, flatten=False, convl=True, drout=True, strides=1, att=False, padding='same'):
+        if expand:
+            x = tf.expand_dims(x, 2)
+        if convl:
+            x = tf.keras.layers.Conv1D(filters, length, strides=strides, padding=padding)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        if att:
+            x = x + tf.keras.layers.Attention()([x, x])
+        if flatten:
+            x = tf.keras.layers.Flatten()(x)
+        if drout:
+            x = tf.keras.layers.Dropout(0.2)(x)
+        return x
+
+    def __init__(self, x_shape):
+        super().__init__()
+        in_data = tf.keras.Input(shape=(x_shape))
+        layer = in_data
+        if cfg.mode == 'technical':
+            layer = tf.keras.layers.Dense(20)(layer)
+            layer = tf.keras.layers.Dropout(0.1)(layer)
+            layer = tf.keras.activations.tanh(layer)
+            layer = tf.keras.layers.BatchNormalization()(layer)
+            layer = tf.keras.layers.Dense(15)(layer)
+            layer = tf.keras.layers.Dropout(0.1)(layer)
+            layer = tf.keras.activations.tanh(layer)
+            layer = tf.keras.layers.BatchNormalization()(layer)
+
+        if cfg.mode == 'diff':
+            # layer = tf.keras.layers.Dense(3)(layer)
+            # layer = tf.keras.activations.relu(layer)
+
+            layer = store_init = tf.expand_dims(layer, 2)
+
+            layer = self.conv(layer, 64, 8)
+            layer = tf.nn.relu(layer)
+            layer = self.conv(layer, 64, 5)
+            layer = tf.nn.relu(layer)
+            layer = self.conv(layer, 64, 3)
+            layer = store = tf.nn.relu(layer)
+
+            layer = tf.keras.layers.Add()([layer, self.conv(store_init, 64, 1)])
+
+            layer = self.conv(layer, 128, 8)
+            layer = tf.nn.relu(layer)
+            layer = self.conv(layer, 128, 5)
+            layer = tf.nn.relu(layer)
+            layer = store2 = self.conv(layer, 128, 3)
+            layer = tf.nn.relu(layer)
+            layer = tf.keras.layers.Add()([layer, self.conv(store, 128, 1)])
+
+            layer = self.conv(layer, 256, 8)
+            layer = tf.nn.relu(layer)
+            layer = self.conv(layer, 256, 5)
+            layer = tf.nn.relu(layer)
+            layer = store3 = self.conv(layer, 256, 3)
+            layer = tf.nn.relu(layer)
+            layer = tf.keras.layers.Add()([layer, self.conv(store2, 256, 1)])
+
+            try:
+                layer = tf.keras.layers.GlobalAveragePooling1D(keepdims=True)(layer)
+            except Exception as e:
+                print('Could not create')
+                layer = tf.keras.layers.GlobalAveragePooling1D()(layer)
+
+            layer = tf.nn.relu(layer)
+
+            layer = tf.keras.layers.LSTM(100, return_sequences=False)(layer)
+
+
+
+            layer = tf.keras.layers.Flatten()(layer)
+
+
+        self.model = tf.keras.Model(inputs=[in_data], outputs=[layer])
+        super().__init__(inputs=[in_data], outputs=[layer])
+        self.compile(optimizer='Adam', loss='mse', metrics=['mse'])
+
+class Decoder(tf.keras.Model):
+    """
+    M_D: merges latent input + previous action to produce the next action.
+    """
+    @staticmethod
+    @tf.function
+    def monitor_loss(_, model_reward_output):
+        return model_reward_output
+
+    @staticmethod
+    def conv(x, filters, length, expand=False, flatten=False, convl=True, drout=True, strides=1, att=False, padding='same'):
+        if expand:
+            x = tf.expand_dims(x, 2)
+        if convl:
+            x = tf.keras.layers.Conv1D(filters, length, strides=strides, padding=padding)(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        if att:
+            x = x + tf.keras.layers.Attention()([x, x])
+        if flatten:
+            x = tf.keras.layers.Flatten()(x)
+        if drout:
+            x = tf.keras.layers.Dropout(0.2)(x)
+        return x
+
+    def __init__(self, layer_shape, activation=tf.keras.layers.LeakyReLU(0.1), K: int = 3):
+        super().__init__()
+        z_in = z_out_layer = tf.keras.Input(shape=(layer_shape))
+
+        prev_action_in = tf.keras.Input((K))
+        merged = tf.concat([z_out_layer, prev_action_in], 1)
+        out = tf.keras.layers.Dense(1)(merged)
+        dec = tf.keras.activations.tanh(out)
+        self.model = tf.keras.Model(inputs=[z_in, prev_action_in], outputs=[dec])
+        super().__init__(inputs=[z_in, prev_action_in], outputs=[dec])
+        self.compile(optimizer='Adam', loss='mse', metrics=[self.monitor_loss])
+
+class UtilityFunction(tf.keras.Model):
+    """
+    Global reward connection: merges (bid,ask) + action vector, returns final reward.
+    """
+    def __init__(self):
+        super().__init__()
+        in_bid_ask = tf.keras.Input((2))
+        in_action = tf.keras.Input((1))
+        cat = tf.expand_dims(tf.concat([in_bid_ask, in_action[:,-1:]], 1), 0)
+        cat = tf.reshape(cat, (-1,3))
+        rew = SharpeRatio()(cat)
+        self.model = tf.keras.Model(inputs=[in_bid_ask, in_action], outputs=[rew])
+        super().__init__(inputs=[in_bid_ask, in_action], outputs=[rew])
+
+class DummyUtilityFunction(tf.keras.Model):
+    """
+    Dummy utility function for inputs of any K returning just average of all inputs
+    """
+    def __init__(self, K: int = 3):
+        super().__init__()
+        in_bid_ask = tf.keras.Input((2))
+        in_action = tf.keras.Input((K))
+        in_action_me = tf.reduce_mean(in_action, axis=1, keepdims=True)
+        cat = tf.expand_dims(tf.concat([in_bid_ask, in_action_me[:,-1:]], 1), 0)
+        cat = tf.reshape(cat, (-1,3))
+        rew = SharpeRatio()(cat)
+        self.model = tf.keras.Model(inputs=[in_bid_ask, in_action], outputs=[rew])
+        super().__init__(inputs=[in_bid_ask, in_action], outputs=[rew])
+
+
+class Agent:
+    """
+    High-level API combining Encoder, Decoder, and UtilityFunction.
+    Trains using offline RRL, online RRL, or RML.
+    K: number of possible actions (3: short, neutral, long).
+    """
+    def __init__(self, encoder=None, decoder=None, utility_function=None, input_len=None, K: int = 3):
+        if None in [encoder, decoder, utility_function]:
+            self.encoder = Encoder(input_len)
+            self.decoder = Decoder(self.encoder.output.shape[1], K=K)
+            self.utility_function = UtilityFunction() if K==3 else DummyUtilityFunction(K=K)
+        else:
+            self.encoder = encoder
+            self.decoder = decoder
+            self.utility_function = utility_function
+        self._phi_X = []
+        self._phi_actions = []
+        self.K = K
+
+    def multiply_decisions(self, ZV):
+        K = int(self.K)
+        ZV = tf.convert_to_tensor(ZV)
+        N = tf.shape(ZV)[0]
+
+        if len(ZV) != int(len(self._phi_X) / K):
+            eyeK = tf.eye(K, dtype=tf.float32)
+            self._phi_actions = tf.repeat(eyeK, repeats=N, axis=0)
+
+        multiples = [K] + [1] * (ZV.shape.rank - 1)
+        self._phi_X = tf.tile(ZV, multiples)
+        return self._phi_X, self._phi_actions
+
+    def phi_processing(self, stacked_preds, initial_action=1):
+        lines = tf.concat(tf.split(tf.round(stacked_preds)+1, self.K), 1)
+        lines_np = lines.numpy().astype(int)
+        decs_ = [initial_action]
+        for row in lines_np:
+            decs_.append(row[decs_[-1]])
+        return tf.one_hot(decs_[:-1],self.K)
+
+    def recurrence_mimicking_forward_pass(self, XV):
+        """
+        Forward step mimicking recurrence of actions 
+        It assumes initial decision
+        """
+        z_out = self.encoder(XV)
+        stacked_z, stacked_a = self.multiply_decisions(z_out)
+        print(stacked_z.shape, stacked_a.shape)
+        stacked_preds = self.decoder([stacked_z, stacked_a])
+        phi_seq = self.phi_processing(stacked_preds)
+        final_dec = self.decoder([z_out, phi_seq])
+        return final_dec
+
+    def compute_apply_grads(self, tape, loss):
+        grad_enc, grad_dec = tape.gradient(loss, [self.encoder.trainable_weights, self.decoder.trainable_weights])
+        grad_enc = [tf.clip_by_value(g, -1000, 1000) for g in grad_enc]
+        grad_dec = [tf.clip_by_value(g, -1000, 1000) for g in grad_dec]
+        self.encoder.optimizer.apply_gradients(zip(grad_enc, self.encoder.trainable_weights))
+        self.decoder.optimizer.apply_gradients(zip(grad_dec, self.decoder.trainable_weights))
+        return grad_enc, grad_dec
+
+
+    def train_iteration(self, XV, BA, offline=False, online_learning=False, count_time=False, forward_only=False, with_gradients = None):
+        if with_gradients is None:
+            with_gradients = not forward_only
+        self.time_counter = []
+        if online_learning:
+            dec_seq = tf.constant([1.])
+            A, B, eta = 0., 0.01, 0.05
+            for i, xrow in enumerate(XV[:-1]):
+                tape_object = nullcontext() if forward_only else tf.GradientTape()
+                with tape_object as tape:
+                    prev_a = tf.one_hot(tf.cast(tf.round(dec_seq[-1:,None]), tf_int),self.K)[0,...]
+                    new_a = self.decoder([self.encoder(xrow[None,:]), prev_a])
+                    dec_seq = tf.concat([dec_seq, new_a[0]], axis=0)
+                    rt = (BA[i+1,0]-BA[i,0]) / BA[i,0] * new_a
+                    dA = rt - A
+                    dB = rt**2 - B
+                    if B - A**2 > 0:
+                        Dt = (B*dA - 0.5*A*dB)/((B - A**2)**1.5)
+                    else:
+                        Dt = 0
+                    loss_value = -Dt
+                    A = A + eta*dA
+                    B = B + eta*dB
+                if not forward_only:
+                    self.compute_apply_grads(tape, loss_value)
+            dec_seq = dec_seq[:-1,None]
+            reward = self.utility_function([BA[:len(dec_seq)], dec_seq])
+            loss_value = -reward
+        elif offline:
+            tape_object = tf.GradientTape() if with_gradients else nullcontext()
+            with tape_object as tape:
+                dec_seq = tf.constant([0.], dtype=tf_float)
+                start = time.time()
+                for i,xrow in enumerate(XV):
+                    prev_a = tf.one_hot(tf.cast(tf.round(dec_seq[-1:,None])+1.0, tf_int),self.K)[0,...]
+
+                    pred_a = self.decoder([self.encoder(xrow[None,:]), prev_a])
+                    
+                    dec_seq = tf.concat([dec_seq, pred_a[0]], axis=0)
+                    if count_time:
+                        self.time_counter.append(time.time()-start)
+
+                ### proper one
+                dec_seq = dec_seq[1:,None]
+
+                reward = self.utility_function([BA, dec_seq])
+                loss_value = -reward
+
+            if not forward_only:    
+                ge, gd = self.compute_apply_grads(tape, loss_value)
+            else:
+                ge, gd = [], []
+            dec_seq = np.round(dec_seq.numpy().reshape(-1,)).astype(int)
+        else:
+            # RML
+            start = time.time()
+
+            tape_object = tf.GradientTape() if with_gradients else nullcontext()
+            with tape_object as tape:
+                dec_seq = self.recurrence_mimicking_forward_pass(XV)
+                reward = self.utility_function([BA, dec_seq])
+                loss_value = -reward
+
+            self.time_counter = [time.time()-start]
+            
+            if not forward_only:
+                ge, gd = self.compute_apply_grads(tape, loss_value)
+            else:
+                ge, gd = [], []
+            start = time.time()
+            dec_seq = np.round(dec_seq.numpy().reshape(-1,)).astype(int) # (tf.argmax(final_dec,1)-1).numpy()
+        return ge+gd, dec_seq, reward, loss_value
+
+    def test_iteration(self, XV, BA, batch_size=10000, offline=False, just_historical_path=False):
+        if offline:
+            dec_seq = tf.constant([1.])
+            for xrow in XV:
+                prev_a = tf.one_hot(tf.cast(tf.round(dec_seq[-1:,None]), tf_int),self.K)[0,...]
+                pred_a = self.decoder([self.encoder(xrow[None,:]), prev_a])
+                dec_seq = tf.concat([dec_seq, tf.cast(tf.argmax(pred_a,1), tf_float)], axis=0)
+            if just_historical_path:
+                return dec_seq
+        else:
+            z_out = self.encoder.predict(XV, batch_size=batch_size, verbose=0)
+            if len(z_out.shape) == 1:
+                z_out = np.expand_dims(z_out, 1)
+            z_out_tf = tf.constant(z_out, tf_float)
+            stacked_z, stacked_a = self.multiply_decisions(z_out_tf)
+            stacked_preds = self.decoder([stacked_z, stacked_a])
+            phi_seq = self.phi_processing(stacked_preds)
+            if just_historical_path:
+                return phi_seq[:-1]
+            dec_seq = tf.argmax(phi_seq,1)-1
+        reward = self.utility_function([BA, self.decoder.predict([z_out, phi_seq], batch_size=batch_size)])
+        loss_value = 1/reward
+        return [], dec_seq, reward, loss_value
+
+    def set_lr(self, lr: float):
+        self.decoder.optimizer.lr.assign(lr)
+        self.encoder.optimizer.lr.assign(lr)
+
+
+    def fit(self, XV, BA, epochs, verbose=0):
+        xv = XV.values
+        ba = BA.values
+        for e in range(epochs):
+            grads, decisions, rewards, loss_value = self.train_iteration(xv, ba
+                                                                            )
+            grads = [sum([x.numpy().reshape(-1,).tolist() for x in grads],[])]
+
+            return_rate = np.round(np.mean(rewards),5) # round((np.prod(rewards)-1)*100,3) if 'sharpe' == 'sharpe' else np.round(np.mean(rewards),5)
+            avg_grads = np.mean(np.abs(sum(grads,[])))
+            if verbose>0:
+                print(return_rate)
+        
+        return self
